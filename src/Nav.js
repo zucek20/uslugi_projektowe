d8d587262c805fe17bb768ca2578ecf019a9774c
zucek <zucek20@gmail.com>
2022-08-20 18:06:50 +0200

    add responsive navigation and NotFound error page

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,14 @@
 import "./styles/Nav.scss"
 import logo from "./img/logo_main.png"
 import { NavLink } from "react-router-dom"
+import burger from "./img/icons/burger.png"
+import { useState } from "react"
 
 export function Nav() {
+  const [active, setActive] = useState(false)
 
   return (
+    <>
     <nav className="navDesktop">
       <div className="navWrap">
 
@@ -19,8 +23,18 @@ export function Nav() {
           <NavLink className="link" to="contact">Kontakt</NavLink>
         </div>
 
-        {/* burger here */}
+        <img src={burger} alt="burger menu" className="burger" onClick={() => setActive(!active)}/>
+
+        
       </div>
     </nav>
+
+    <div id="menu" className={active.toString()}>
+      <NavLink onClick={() => setActive(!active)} className="link" to="/">Oferta</NavLink>
+      <NavLink onClick={() => setActive(!active)} className="link" to="projects">Projekty</NavLink>
+      <NavLink onClick={() => setActive(!active)} className="link" to="about">O mnie</NavLink>
+      <NavLink onClick={() => setActive(!active)} className="link" to="contact">Kontakt</NavLink>
+    </div>
+    </>
   )
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,19 @@
+import { NavLink } from "react-router-dom"
+import Animated from "../Animated"
+import robot from "../img/icons/robot.png"
+import "../styles/NotFound.scss"
+
 export function NotFound() {
   return (
-    <h1>Błąd 404</h1>
+    <Animated>
+      <section className="notFound">
+        <img src={robot} alt="robot" />
+        <h2>Błąd 404</h2>
+
+        <p>Nie odnaleziono strony</p>
+
+        <NavLink className="back" to="/">Strona główna</NavLink>
+      </section>
+    </Animated>
   )
 }
