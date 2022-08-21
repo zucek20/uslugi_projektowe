84b6d0bf1efec541d5352467c958feb98446a960
zucek <zucek20@gmail.com>
2022-08-21 17:32:25 +0200

    Add responsive styles for Contact & About subpages

--- src/Nav.js
+++ src/Nav.js
@@ -2,10 +2,17 @@ import "./styles/Nav.scss"
 import logo from "./img/logo_main.png"
 import { NavLink } from "react-router-dom"
 import burger from "./img/icons/burger.png"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function Nav() {
   const [active, setActive] = useState(false)
+
+  const scroll = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  
 
   return (
     <>
@@ -13,27 +20,27 @@ export function Nav() {
       <div className="navWrap">
 
         <NavLink to="/">
-          <img className="logo_main" src={logo} alt="logo"/>
+          <img onClick={setTimeout(() => scroll(), 300)} className="logo_main" src={logo} alt="logo"/>
         </NavLink>
 
         <div className="links">
-          <NavLink className="link" to="/">Oferta</NavLink>
-          <NavLink className="link" to="projects">Projekty</NavLink>
-          <NavLink className="link" to="about">O mnie</NavLink>
-          <NavLink className="link" to="contact">Kontakt</NavLink>
+          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="/">Oferta</NavLink>
+          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="projects">Projekty</NavLink>
+          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="about">O mnie</NavLink>
+          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="contact">Kontakt</NavLink>
         </div>
 
         <img src={burger} alt="burger menu" className="burger" onClick={() => setActive(!active)}/>
-
+        
         
       </div>
     </nav>
 
     <div id="menu" className={active.toString()}>
-      <NavLink onClick={() => setActive(!active)} className="link" to="/">Oferta</NavLink>
-      <NavLink onClick={() => setActive(!active)} className="link" to="projects">Projekty</NavLink>
-      <NavLink onClick={() => setActive(!active)} className="link" to="about">O mnie</NavLink>
-      <NavLink onClick={() => setActive(!active)} className="link" to="contact">Kontakt</NavLink>
+      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="/">Oferta</NavLink>
+      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="projects">Projekty</NavLink>
+      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="about">O mnie</NavLink>
+      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="contact">Kontakt</NavLink>
     </div>
     </>
   )
