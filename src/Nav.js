96d5ac6c0717dfc803b838304b541bb5446e48fb
zucek <zucek20@gmail.com>
2022-08-21 17:50:30 +0200

    refactor scrolling nav script

--- src/Nav.js
+++ src/Nav.js
@@ -7,12 +7,12 @@ import { useEffect, useState } from "react"
 export function Nav() {
   const [active, setActive] = useState(false)
 
-  const scroll = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  function timeoutScroll() {
+    setTimeout(() => {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }, 300)
   }
-
-  
 
   return (
     <>
@@ -20,27 +20,26 @@ export function Nav() {
       <div className="navWrap">
 
         <NavLink to="/">
-          <img onClick={setTimeout(() => scroll(), 300)} className="logo_main" src={logo} alt="logo"/>
+          <img onClick={function() {timeoutScroll()}} className="logo_main" src={logo} alt="logo"/>
         </NavLink>
 
         <div className="links">
-          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="/">Oferta</NavLink>
-          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="projects">Projekty</NavLink>
-          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="about">O mnie</NavLink>
-          <NavLink onClick={function(){setTimeout(() => scroll(), 300)}} className="link" to="contact">Kontakt</NavLink>
+          <NavLink onClick={function(){timeoutScroll()}} className="link" to="/">Oferta</NavLink>
+          <NavLink onClick={function(){timeoutScroll()}} className="link" to="projects">Projekty</NavLink>
+          <NavLink onClick={function(){timeoutScroll()}} className="link" to="about">O mnie</NavLink>
+          <NavLink onClick={function(){timeoutScroll()}} className="link" to="contact">Kontakt</NavLink>
         </div>
 
         <img src={burger} alt="burger menu" className="burger" onClick={() => setActive(!active)}/>
-        
         
       </div>
     </nav>
 
     <div id="menu" className={active.toString()}>
-      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="/">Oferta</NavLink>
-      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="projects">Projekty</NavLink>
-      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="about">O mnie</NavLink>
-      <NavLink onClick={() => {setActive(!active); setTimeout(() => {scroll()}, 300)}} className="link" to="contact">Kontakt</NavLink>
+      <NavLink onClick={() => {setActive(!active); timeoutScroll()}} className="link" to="/">Oferta</NavLink>
+      <NavLink onClick={() => {setActive(!active); timeoutScroll()}} className="link" to="projects">Projekty</NavLink>
+      <NavLink onClick={() => {setActive(!active); timeoutScroll()}} className="link" to="about">O mnie</NavLink>
+      <NavLink onClick={() => {setActive(!active); timeoutScroll()}} className="link" to="contact">Kontakt</NavLink>
     </div>
     </>
   )
